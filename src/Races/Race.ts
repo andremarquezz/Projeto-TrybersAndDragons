81c00dd1f7f778f058e3ee647dbb5b955121eacd
jey <andre.marqquez@gmail.com>
2022-09-22 15:22:12 -0300

    cria classe Race e set dos atributos

---
+++ src/Races/Race.ts
@@ -0,0 +1,11 @@
+abstract class Race {
+  constructor(private _name: string, private _dexterity: number) { }
+  
+  public get name(): string {
+    return this._name;
+  }
+  
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+}
