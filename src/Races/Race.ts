a9918d6f297775fafddb224aed8a034044284e12
Jey <andre.marqquez@gmail.com>
2022-09-22 17:05:51 -0300

    feat [class] : Adiciona superclass Race
    Adiciona superclass Race com atributos e metodos para as subclasses

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,11 +1,19 @@
 abstract class Race {
-  constructor(private _name: string, private _dexterity: number) { }
-  
+  constructor(private _name: string, private _dexterity: number) {}
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+
   public get name(): string {
     return this._name;
   }
-  
+
   public get dexterity(): number {
     return this._dexterity;
   }
 }
+
+export default Race;
