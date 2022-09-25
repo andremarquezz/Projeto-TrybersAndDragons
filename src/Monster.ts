21e8f41a558c2360d2f78481af179b7675d42d3e
Jey <andre.marqquez@gmail.com>
2022-09-25 17:44:18 -0300

    feat [Monster]: Cria classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,37 @@
+import Fighter from './Fighter';
+import SimpleFighter from './SimpleFighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const receivedDamage = attackPoints - this._lifePoints;
+    if (receivedDamage > 0) {
+      this._lifePoints -= receivedDamage;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+}
+
+export default Monster;
